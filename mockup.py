eaa6760ab1eac63088417241c55797847340846d
Luna Burrows <lunasnowtaken@gmail.com>
2026-08-26 20:26:37 +1000

    Added some internal docs and some other fallback fonts

--- mockup.py
+++ mockup.py
@@ -17,7 +17,6 @@ from PyQt6.QtWidgets import (
     QCheckBox,
 )
 
-
 # class TodoLineEdit(QLineEdit):
 #     def __init__(self, parent=None):
 #         super().__init__(parent)
@@ -32,6 +31,8 @@ from PyQt6.QtWidgets import (
 #             self.setToolTip(self.text())
 #         else:
 #             self.setToolTip("")
+
+
 class NoteTextEdit(QTextEdit):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -51,21 +52,36 @@ class NoteTextEdit(QTextEdit):
 
 
 class Clock(QLabel):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.update_clock()
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_clock)
         self.timer.start(1000)
 
-    def update_clock(self):
+    def update_clock(self) -> None:
+        """Updates the clock"""
         self.setText(self.clock_system())
 
-    def clock_system(self):
+    def clock_system(self) -> str:
+        """Returns the current time in a user friendly format as a string"""
+        # pythons time module "string formatted time" which returns the current time using the specified format
+        # <hours>:<minutes>:<seconds>
         return t.strftime("%H:%M:%S")
 
 
-def create_stopwatch_string(h, m, s, ms):
+def create_stopwatch_string(h: int, m: int, s: int, ms: int) -> str:
+    """takes 4 ints; hours, minutes, seconds, and milliseconds, and formats them into an appropriate string for display
+
+    Args:
+        h (int): Hours
+        m (int): Minutes
+        s (int): Seconds
+        ms (int): Milliseconds
+
+    Returns:
+        str: A user friendly formatted string
+    """
     hours = False
     string = ""
     if h:
@@ -82,12 +98,13 @@ def create_stopwatch_string(h, m, s, ms):
 
 
 class Stopwatch(QWidget):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         layout = QHBoxLayout()
         layout.setSpacing(20)
         self.is_running = False
 
+        # define a bunch of variables and set them all to 0
         (
             self.ms,
             self.current_secs,
@@ -98,31 +115,40 @@ class Stopwatch(QWidget):
             self.last_offset_secs,
         ) = (0, 0, 0, 0, 0, 0, 0)
 
+        # Every 10ms update the stopwatch display
         self.timer = QTimer(self)
         self.timer.setInterval(10)
         self.timer.timeout.connect(self.update_label)
 
+        # Define a start/stop button for the stopwatch, and connect it to the control function
         self.start_button = QPushButton("Start")
         self.start_button.clicked.connect(self.control)
 
+        # define the default "zero" state of the stopwatch
         self.default_timer_text = "00.00"
         self.timed = QLabel(self.default_timer_text)
 
+        # define a restart button and connect it to the reset function
         self.restart_button = QPushButton("Reset")
         self.restart_button.clicked.connect(self.reset)
 
+        # define button sizes
         self.start_button.setMaximumSize(50, 50)
         self.restart_button.setMaximumSize(50, 50)
 
+        # add the widgets
         layout.addWidget(self.start_button)
         layout.addWidget(self.timed, alignment=Qt.AlignmentFlag.AlignCenter, stretch=2)
         layout.addWidget(self.restart_button, 1)
 
         self.setLayout(layout)
 
-    def control(self):
+    def control(self) -> None:
+        """Stops the stopwatch if it's running, starts it if it's not"""
         if not self.is_running:
             if self.start_secs == 0:
+                # if start secs is zero, this function is being called after resetting the stopwatch, and we should define the start time
+                # if we didn't do this conditionally, pausing and playing the timer (which calls this function where start secs is already defined) would reset it
                 self.start_secs = int(t.time())
             self.timer.start()
             self.start_button.setText("Stop")
@@ -131,7 +157,8 @@ class Stopwatch(QWidget):
             self.start_button.setText("Start")
             self.is_running = False
 
-    def reset(self):
+    def reset(self) -> None:
+        # reset all the dependent variables
         (
             self.ms,
             self.current_secs,
@@ -141,14 +168,36 @@ class Stopwatch(QWidget):
             self.elapsed_secs,
             self.last_offset_secs,
         ) = (0, 0, 0, 0, 0, 0, 0)
+
+        # reset the stopwatch display
         self.timed.setText(self.default_timer_text)
         self.timer.stop()
         self.is_running = False
         self.start_button.setText("Start")
 
-    def update_label(self):
+    def update_label(self) -> None:
+        # this is the core timer logic
+        # the logic works by defining when we started the timer as seconds since epoch,
+        # and then comparing that to what time it is now, finding the difference,
+        # and then converting that time delta from seconds to hours, minutes, and seconds.
+        # we get milliseconds by realising that this function is called every 10ms,
+        # so we can count every time this function is called.
+        # playing and pausing is handled by, when the stopwatch is paused,
+        # counting how long its been paused along with how long its been active
+        # and then we can find out what time to display by subtracting time spent paused from time active.
+
+        # we do all this instead of just counting up every 10ms, as the time between cycles is
+        # the 10ms spent sleeping plus however long this logic takes, which might be up to a ms or 2.
+        # because of this, little inaccuracies build up and we get a slow stopwatch.
+        # we can't just sleep 9ms and count on 1ms of logic, as this logic will run faster or slower on different machines.
+
+        # this function called every 10ms, so add 10 to ms every 10(ish)ms
         self.ms = (self.ms + 10) % 1000
+
+        # find out what time were currently at
         self.current_secs = int(t.time())
+
+        # if the timer isn't running (is paused), count the running total of how long it's spent paused
         if not self.is_running:
             self.offset_secs = (
                 self.current_secs
@@ -157,32 +206,45 @@ class Stopwatch(QWidget):
                 + self.last_offset_secs
             )
         else:
+            # we have to keep track of the running total of time spent paused, thus this logic
             self.last_offset_secs = self.offset_secs
+
+            # elapsed time equals what time since epoch we're currently at minus when we started
             self.elapsed_secs = self.current_secs - self.start_secs
 
+        # take into account the offset given by the stopwatch being paused, and modulo 60 to convert say 100s to 40s and 1m
         secs = (self.elapsed_secs - self.offset_secs) % 60
+
+        # remove any compounding inaccuracy in ms by setting ms to 0 at every change in number of seconds
         if self.last_secs != secs:
-            # remove any compounding inaccuracy in ms by setting ms to 0 at every change in seconds
             self.ms = 0
-            
+
         self.last_secs = secs
+
+        # 60 seconds in a minute, taking into account offset, modulo 60 to get minutes and hours
         mins = ((self.elapsed_secs - self.offset_secs) // 60) % 60
+        # 3600 seconds in an hour, taking into account offset,
+        # no modulo as we dont have logic for days, so seeing values >23 hrs is fine
         hours = (self.elapsed_secs - self.offset_secs) // 3600
 
+        # only update stopwatch if it's running
         if self.is_running:
             self.timed.setText(create_stopwatch_string(hours, mins, secs, self.ms))
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setFixedWidth(400)
         self.move(0, 0)
+        # this adds the following ttf file font to the environment if the file exists
         QFontDatabase.addApplicationFont("LCD5X8H.TTF")
+        # otherwise, it the font isn't installed to system or present at project root,
+        # this css will fallback to a bunch of other likely monospace fonts, ranked preferentially.
         self.setStyleSheet("""
         QLabel {
             font-size: 30pt;
-            font-family: LCD5x8H, "Courier New", Courier, monospace;
+            font-family: LCD5x8H, Consolas, "Cascadia Code", "Cascadia Mono", Inconsolata, "Lucida Console", "Courier New", Courier, monospace;
             }
         """)
         self.header_layout = QVBoxLayout()
@@ -233,14 +295,14 @@ class MainWindow(QMainWindow):
         self.current_note_row = 1
         self.current_todo_row = 0
 
-    def add_note(self):
+    def add_note(self) -> None:
         new_note = NoteTextEdit()
         new_note.setPlaceholderText("Type something here...")
         self.layout.addWidget(new_note, self.current_note_row, 0)
 
         self.current_note_row += 1
 
-    def add_todo(self):
+    def add_todo(self) -> None:
         new_checkbox = QCheckBox()
         new_line = NoteTextEdit()
         self.todo_layout.addWidget(new_checkbox, self.current_todo_row, 0)
